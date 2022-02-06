d49430f951fce818517bed84fc413e552f4bc05a
Pravaritti <pravaritti.1995@gmail.com>
2022-02-06 11:24:26 +0530

    switch the active player

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 'use strict';
 
+//Selceting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
@@ -14,6 +17,7 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -29,8 +33,19 @@ btnRoll.addEventListener('click', function () {
   //3. Check for rolled if 1: switch to next player
   if (dice != 1) {
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+
+    //current0El.textContent = currentScore;
   } else {
     //Switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+
+    //change background color of playing player
+    //toggle method = if a class is there then remove and vice vers
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
   }
 });
